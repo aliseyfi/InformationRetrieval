b6414017deb6c1f2589639cf6960e6daf704d1b4
Eli Andrew <eliandrew@Elis-MacBook-Pro.local>
2017-05-06 15:02:06 -0400

    Added Score class

---
+++ Score.py
@@ -0,0 +1,12 @@
+
+class Score:
+
+    def __init__(self, source_a, source_b, feature):
+        self.source_a = source_a
+        self.source_b = source_b
+        self.feature = feature
+        self.score = 0
+
+    def calculate_score(self):
+        self.score = 0
+        return self.score
