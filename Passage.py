1bcd31d4161bb3eeee691837c8a925658d32be5a
Katherine Walker <walker13k@gmail.com>
2017-05-09 11:34:07 -0400

    Add get_passage and get_sentecne

---
+++ Passage.py
@@ -0,0 +1,22 @@
+from Source import *
+
+
+# Subclass of Source - text is raw text from document
+# - text: raw string from passage
+# - scores: list of Score objects between this document and all queries
+# - sentences: list of Sentence objects that hold all sentences from this passage
+class Passages:
+
+    def __init__(self, text, features):
+        Source.__init__(self, features)
+        self.text = text
+        self.scores = []
+        self.sentences = get_sentences(self)
+
+
+    # Katherine: get_passages()
+    # - returns list of Sentence objects and stores them in self.sentences
+    def get_sentences(self):
+        split_sentences = re.split('[.!?]', self.text)
+        for sentence in split_sentences:
+            self.sentences.append(Sentence(text = sentence, features = self.features)
