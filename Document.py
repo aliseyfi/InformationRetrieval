7cad5438949d54269cffedfd70edf1514f4669f9
Eli Andrew <eliandrew@Elis-MacBook-Pro.local>
2017-05-09 11:20:53 -0400

    Updated with new design

--- Document.py
+++ Document.py
@@ -1,11 +1,29 @@
 from Source import *
 
 
-# Subclass of Source - text is derived from the given URL
-# - url: web url where Source text can be found
+# Subclass of Source - text is raw text from document
+# - text: raw string from document
+# - scores: list of Score objects between this document and all queries
+# - passages: list of Passage objects that hold all passages from this document
 class Document:
 
-    def __init__(self, url, features):
+    def __init__(self, text, features):
         Source.__init__(self, features)
-        self.url = url
+        self.text = text
+        self.scores = []
+        self.passages = []
 
+
+    # Katherine: get_passages()
+    # - returns list of Passage objects and stores them in self.passages
+
+
+    # Returns subset of self.passages with top n scoring passages
+    def get_highest_passages(self, n):
+        highest_passages = []
+        for query_index in range(len(self.scores)):
+            highest_passages.append(self.highest_passages_for_query(n, query_index))
+
+    def highest_passages_for_query(self, n, query_index):
+        highest_passages = sorted(self.passages, key=lambda passage: passage.scores[query_index])
+        return highest_passages[:n]
